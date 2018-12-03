75fc5c60acab208c347e92e58752d37299e88c75
bill <bill@debian-machine.bill.local>
2018-12-03 04:44:37 +0000

    added better camera movement

--- src/common/controls.cpp
+++ src/common/controls.cpp
@@ -1,9 +1,13 @@
 
 #include "controls.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtx/norm.hpp>
 
 glm::mat4 ViewMatrix;
 glm::mat4 ProjectionMatrix;
 glm::vec3 position = glm::vec3(0, -20, 0);
+float camAngleX = 0, camAngleY = 0;
+float distanceFromCentre = 100.0f;
 float initialFoV = 45.0f;
 
 void getMatrices(mat4 &projectionMatrix, mat4 &viewMatrix, vec3 &cameraPosition, mat4 &viewProjectionMatrix) {
@@ -16,21 +20,33 @@ void getMatrices(mat4 &projectionMatrix, mat4 &viewMatrix, vec3 &cameraPosition,
 glm::vec3 centerOfParticleSystem = glm::vec3(0.0f, 0.0f, 0.0f);
 
 void computeMatricesFromInputs(GLFWwindow *window) {
-    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS) {
-        position.x++;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+        camAngleX--;
     }
 
-    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS) {
-        position.x--;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+        camAngleX++;
     }
 
-    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
-        position.z++;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        camAngleY--;
     }
 
-    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
-        position.z--;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+        camAngleY++;
     }
+
+    if (glfwGetKey(window, GLFW_KEY_KP_ADD) == GLFW_PRESS) {
+        distanceFromCentre--;
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_KP_SUBTRACT) == GLFW_PRESS) {
+        distanceFromCentre++;
+    }
+
+    position.x = distanceFromCentre * -sinf(camAngleX * (M_PI/180)) * cosf(camAngleY * (M_PI/180) );
+    position.y = -distanceFromCentre * cosf(camAngleX * (M_PI/180)) * cosf(camAngleY * (M_PI/180) );
+    position.z = distanceFromCentre * -sinf(camAngleY * (M_PI/180));
 
     glm::vec3 direction = centerOfParticleSystem - position;
 
@@ -40,5 +56,5 @@ void computeMatricesFromInputs(GLFWwindow *window) {
 
     // Projection matrix : 45� Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
     ProjectionMatrix = glm::perspective(glm::radians(FoV), 4.0f / 3.0f, 0.1f, 10000.0f);
-    ViewMatrix = glm::lookAt(position, position + direction, up);
+    ViewMatrix = glm::lookAt(position, centerOfParticleSystem, up);
 }
